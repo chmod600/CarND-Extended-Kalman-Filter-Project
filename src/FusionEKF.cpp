f7e14ab6a5a51b5c3101616abd73705f7cedad35
Akshay Khole <akshay@reversehack.in>
2018-04-22 13:10:51 -0700

    Minor uniformity improvements

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -88,16 +88,16 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       */
       float ro = measurement_pack.raw_measurements_[0];
       float theta = measurement_pack.raw_measurements_[1];
-      ekf_.x_(0) = ro * cos(theta);
-      ekf_.x_(1) = ro * sin(theta);
+      ekf_.x_(0) = ro * cos(theta); // px
+      ekf_.x_(1) = ro * sin(theta); // py
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
       Initialize state.
       */
 
-      ekf_.x_(0) = measurement_pack.raw_measurements_[0];
-      ekf_.x_(1) = measurement_pack.raw_measurements_[1];
+      ekf_.x_(0) = measurement_pack.raw_measurements_[0]; // px
+      ekf_.x_(1) = measurement_pack.raw_measurements_[1]; // py
     }
 
     previous_timestamp_ = measurement_pack.timestamp_;
@@ -119,18 +119,19 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
 
-  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
+  // dt - expressed in seconds
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;
   previous_timestamp_ = measurement_pack.timestamp_;
 
   float dt_2 = dt * dt;
   float dt_3 = dt_2 * dt;
   float dt_4 = dt_3 * dt;
 
-  //Modify the F matrix so that the time is integrated
+  // Modify the F matrix so that the time is integrated
   ekf_.F_(0, 2) = dt;
   ekf_.F_(1, 3) = dt;
 
-  // set the process covariance matrix Q
+  // Set the process covariance matrix Q
   ekf_.Q_ = MatrixXd(4, 4);
   ekf_.Q_ <<  dt_4 / 4 * noise_ax, 0, dt_3 / 2 * noise_ax, 0,
     0, dt_4 / 4 * noise_ay, 0, dt_3 / 2 * noise_ay,
@@ -156,8 +157,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   } else {
     // Laser updates
-    ekf_.H_ = H_laser_;
     ekf_.R_ = R_laser_;
+    ekf_.H_ = H_laser_;
     ekf_.Update(measurement_pack.raw_measurements_);
   }
 
