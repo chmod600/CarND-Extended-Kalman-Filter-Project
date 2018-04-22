7bee145305ab93f703bb41015711a6f07fa85fb5
Akshay Khole <akshay@reversehack.in>
2018-04-22 02:58:25 -0700

    Updates namespacing

--- src/tools.cpp
+++ src/tools.cpp
@@ -9,15 +9,7 @@ Tools::Tools() {}
 
 Tools::~Tools() {}
 
-// VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-//                               const vector<VectorXd> &ground_truth) {
-//   /**
-//   TODO:
-//     * Calculate the RMSE here.
-//   */
-// }
-
-VectorXd CalculateRMSE(const vector<VectorXd> &estimations,
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                        const vector<VectorXd> &ground_truth){
 
   VectorXd rmse(4);
@@ -26,8 +18,7 @@ VectorXd CalculateRMSE(const vector<VectorXd> &estimations,
   // check the validity of the following inputs:
   //  * the estimation vector size should not be zero
   //  * the estimation vector size should equal ground truth vector size
-  if(estimations.size() != ground_truth.size()
-     || estimations.size() == 0){
+  if(estimations.size() != ground_truth.size() || estimations.size() == 0){
     cout << "Invalid estimation or ground_truth data" << endl;
     return rmse;
   }
@@ -52,15 +43,7 @@ VectorXd CalculateRMSE(const vector<VectorXd> &estimations,
   return rmse;
 }
 
-
-// MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-//   /**
-//   TODO:
-//     * Calculate a Jacobian here.
-//   */
-// }
-
-MatrixXd CalculateJacobian(const VectorXd& x_state) {
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
 
   MatrixXd Hj(3,4);
   //recover state parameters
